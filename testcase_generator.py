007fcd87f31c3f518b183666355ae5ad62149efb
ksdfg <ksdfg123@gmail.com>
2019-08-01 20:10:44 +0530

    forgot to make the generator more generic

--- testcase_generator.py
+++ testcase_generator.py
@@ -1,48 +1,34 @@
 import random
 
 for i in range(100000):
+
+    # generate a string value test which is input string by doing stuff
     test = ''
 
-    n = random.randint(0, 5)
-    test += str(n) + '\n'
-
-    arr = list()
-    for j in range(n):
-        meow = random.randint(-256, 255)
-        test += str(meow) + ' '
-        arr.append(meow)
-
-    test = test[:-1]
-
+    # the weird multiline comment is just to make it easier to switch between
+    # testing the logic and writing to files
     '''print(test)
     r'''
     f = open(r'C:\Users\ksdfg\Desktop\rsc\input\input' + str(i) + '.txt', 'w')
     f.write(test)
     #'''
 
+
+    # generate a string value res which is output string by doing stuff
     res = ''
 
-    index = list()
-    values = list()
-    for j in range(len(arr)):
-        if arr[j] < 0 and arr[j] % 2 != 0 or arr[j] >= 0 and arr[j] % 2 == 0:
-            index.append(j)
-            values.append(arr[j])
-
-    values.sort()
-
-    meow = 0
-    for j in index:
-        arr[j] = values[meow]
-        meow += 1
-
-    for j in arr:
-        res += str(j) + ' '
-
-    #res = res[:-1]
-
+    # the weird multiline comment is just to make it easier to switch between
+    # testing the logic and writing to files
     '''print(res)
     r'''
     f = open(r'C:\Users\ksdfg\Desktop\rsc\output\output' + str(i) + '.txt', 'w')
     f.write(res)
     #'''
+
+'''
+The format of the zip file is that it should have two folders - input (with all
+the input test files) and output (with all the output test files). akhilnarang,
+zip the two folders input and output (in C:\Users\ksdfg\Desktop\rsc for me)
+into a single zip file that is to be uploaded.
+Someone check out if there's a hackerrank api for uploading this zip file.
+'''
