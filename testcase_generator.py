4d2cf757bf4cf512f05005e167d61de09a01c0f5
ksdfg <ksdfg123@gmail.com>
2020-04-11 22:29:26 +0530

    version 2020

--- testcase_generator.py
+++ testcase_generator.py
@@ -1,39 +1,45 @@
 import logicfile
 import zipfile
 import os
+from subprocess import Popen, PIPE, STDOUT
+
+to_write = False
 
 for i in range(1):
-    # generate a dictionary which has input string by doing stuff
-    testDict = logicfile.inputLogic()
-    test = testDict['string']
+    # generate input string by doing stuff
+    test, name = logicfile.inputLogic()
 
-    # the weird multiline comment is just to make it easier to switch between
-    # testing the logic and writing to files
-    print('\ninput :\n', test, sep='\n')
-    r'''
-    f = open(r'path\to\input\input' + str(i) + '.txt', 'w')
-    f.write(test)
-    # '''
+    if to_write:
+        f = open(r'path/to/folder/input' + str(i) + '.txt', 'w')
+        f.write(test)
+    else:
+        print('\ninput :\n', test, sep='\n')
 
-    # generate a string value res which is output string by doing stuff
-    res = logicfile.outputLogic(testDict)
+    # generate a string value res which is output string by passing input string to solution file
+    res = (
+        Popen(
+            ['command', 'path/to/solution/file'],
+            stdout=PIPE,
+            stdin=PIPE,
+            stderr=STDOUT,
+        ).communicate(input=bytes(test, 'utf-8'))[0]
+    ).decode('utf-8')
 
-    # the weird multiline comment is just to make it easier to switch between
-    # testing the logic and writing to files
-    print('\noutput :\n', res, sep='\n')
-    r'''
-    f = open(r'path\to\output\output' + str(i) + '.txt', 'w')
-    f.write(res)
+    if to_write:
+        f = open(r'path/to/output/output' + str(i) + '.txt', 'w')
+        f.write(res)
+    else:
+        print('\noutput :\n', res, sep='\n')
 
-# write all test cases (input and output) to zipfile
-with zipfile.ZipFile(r'path\to\fib.zip', 'w', zipfile.ZIP_DEFLATED) as zip_file:
-    # walk through input folder, and write all files to zip file as 'input\inputx.txt'
-    for root, directories, files in os.walk(r'path\to\input'):
-        for file in files:
-            zip_file.write(os.path.join(root, file), os.path.join('input', file))
+if to_write:
+    # write all test cases (input and output) to zipfile
+    with zipfile.ZipFile(f'path/to/{name}.zip', 'w', zipfile.ZIP_DEFLATED) as zip_file:
+        # walk through input folder, and write all files to zip file as 'input\inputx.txt'
+        for root, directories, files in os.walk(r'path/to/input'):
+            for file in files:
+                zip_file.write(os.path.join(root, file), os.path.join('input', file))
 
-    # walk through output folder, and write all files to zip file as 'output\outputx.txt'
-    for root, directories, files in os.walk(r'path\to\output'):
-        for file in files:
-            zip_file.write(os.path.join(root, file), os.path.join('output', file))
-# '''
+        # walk through output folder, and write all files to zip file as 'output\outputx.txt'
+        for root, directories, files in os.walk(r'path/to/output'):
+            for file in files:
+                zip_file.write(os.path.join(root, file), os.path.join('output', file))
