f0d6a81de47db69abea79f2c4839ff24bad368c7
ksdfg <ksdfg123@gmail.com>
2020-04-11 23:00:31 +0530

    store testcase folder as a var for easy edits

--- logicfile.py
+++ logicfile.py
@@ -3,8 +3,13 @@ Add functions that are going to have your input generation logic here.
 Let these functions return the string we need to write to file, and the name of the zip file to be generated.
 """
 
-from random import randint
+from random import randint, choice
 
 
 def inputLogic():
     return "meow", 'nyan'
+
+
+def bov1():
+    var = "".join([choice("DOPE") for _ in range(randint(0, 10000))])
+    return str(len(var)) + "\n" + var, 'bov1'
--- testcase_generator.py
+++ testcase_generator.py
@@ -5,13 +5,15 @@ from subprocess import Popen, PIPE, STDOUT
 
 to_write = False
 
+testcase_folder = "path/to/folder"
+
 for i in range(1):
     # generate input string by doing stuff
-    test, name = logicfile.inputLogic()
+    test, name = logicfile.bov1()
 
     if to_write:
-        f = open(r'path/to/folder/input' + str(i) + '.txt', 'w')
-        f.write(test)
+        with open(f'{testcase_folder}/input/input' + str(i) + '.txt', 'w') as f:
+            f.write(test)
     else:
         print('\ninput :\n', test, sep='\n')
 
@@ -26,20 +28,22 @@ for i in range(1):
     ).decode('utf-8')
 
     if to_write:
-        f = open(r'path/to/output/output' + str(i) + '.txt', 'w')
-        f.write(res)
+        with open(f'{testcase_folder}/output/output' + str(i) + '.txt', 'w') as f:
+            f.write(res)
     else:
         print('\noutput :\n', res, sep='\n')
 
 if to_write:
     # write all test cases (input and output) to zipfile
-    with zipfile.ZipFile(f'path/to/{name}.zip', 'w', zipfile.ZIP_DEFLATED) as zip_file:
+    with zipfile.ZipFile(
+        f'{testcase_folder}/{name}.zip', 'w', zipfile.ZIP_DEFLATED
+    ) as zip_file:
         # walk through input folder, and write all files to zip file as 'input\inputx.txt'
-        for root, directories, files in os.walk(r'path/to/input'):
+        for root, directories, files in os.walk(f'{testcase_folder}/input'):
             for file in files:
                 zip_file.write(os.path.join(root, file), os.path.join('input', file))
 
         # walk through output folder, and write all files to zip file as 'output\outputx.txt'
-        for root, directories, files in os.walk(r'path/to/output'):
+        for root, directories, files in os.walk(f'{testcase_folder}/output'):
             for file in files:
                 zip_file.write(os.path.join(root, file), os.path.join('output', file))
